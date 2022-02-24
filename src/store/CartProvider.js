cb415d4056054ab5c9f210eaea4c3f17473980cc
smadjapa <smadjapa@gmail.com>
2022-02-24 23:21:57 +0300

    Adding the cart context

---
+++ src/store/CartProvider.js
@@ -0,0 +1,27 @@
+import CartContext from "./cart-context";
+
+const CartProvider = props => {
+
+    const addItemToCartHandler = item => {
+
+    };
+
+    const removeItemFromCartHandler = itemId => {
+
+    }
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler
+    }
+
+    return(
+        <CartContext.Provider vlaue={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartProvider;
