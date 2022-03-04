0fccc581661ae9b3fc9b4e0bd0d54a89a2974057
smadjapa <smadjapa@gmail.com>
2022-03-04 12:55:21 +0300

    Implementing removing of the item from the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -17,10 +17,10 @@ const cartReducer = (state, action) => {
         );
 
         const existingCartItem = state.items[existingCartItemIndex];
-        
+
         let updatedItems;
 
-        if(existingCartItem){
+        if (existingCartItem) {
             const updatedItem = {
                 ...existingCartItem,
                 amount: existingCartItem.amount + action.item.amount
@@ -38,6 +38,30 @@ const cartReducer = (state, action) => {
     }
 
     if (action.type === 'REMOVE') {
+
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.id
+        );
+
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+
+
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return{
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
 
     }
 
