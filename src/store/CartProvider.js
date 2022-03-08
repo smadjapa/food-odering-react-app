690d5c777b9cf6eb5d3bf86bd25ba43d533087e8
smadjapa <smadjapa@gmail.com>
2022-03-08 23:44:16 +0300

    Clearing the cart aafter posting the data

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -61,8 +61,12 @@ const cartReducer = (state, action) => {
         return{
             items: updatedItems,
             totalAmount: updatedTotalAmount
-        }
+        };
 
+    }
+
+    if (action.type === 'CLEAR') {
+        return defaultCartState;
     }
 
     return defaultCartState;
@@ -81,11 +85,16 @@ const CartProvider = props => {
         dispatchCartAction({ type: 'REMOVE', id: itemId });
     }
 
+    const clearCartHandler = () => {
+        dispatchCartAction({ type: 'CLEAR'});
+    }
+
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
+        removeItem: removeItemFromCartHandler,
+        clearCart: clearCartHandler
     }
 
     return (
